159252ddc35b2a4804c93fa1ca27d821106cd733
Hagaaty Hagaaty <hagaaty@gmail.com>
2026-04-01 18:09:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2808088129-6bbd8",
+  "appId": "1:857120473794:web:dd1b388940cd177a69b15b",
+  "apiKey": "AIzaSyD8oEnPdOsHF8uc9ZSnaPouzJUlSZDPZhQ",
+  "authDomain": "studio-2808088129-6bbd8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "857120473794"
+};
